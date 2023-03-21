a2b6edc8e8f29b5bf49a4264196372d9a45a0e81
Elan-Gr <55502345+Elan-Gr@users.noreply.github.com>
2023-03-21 17:59:40 -0500

    Started parser outline

---
+++ sw/parser/parser.c
@@ -0,0 +1,41 @@
+#include "parser.h"
+int main(int argc, char *argv[]){
+
+
+
+
+    return 0;
+}
+
+// Input: User input string
+// Output: Array of Tokens in reverse polish notation
+void parse_equation(char * userInput, Token * output){
+    // Convert input to array of tokens
+    Token userInputTokens[100];
+    text_to_array_of_tokens(userInput, userInputTokens);
+
+    // Convert array of tokens to reverse polish notation
+    to_reverse_polish_notation(userInputTokens, output);
+}
+
+
+// Parsed using the Shunting yard algorithm
+// https://en.wikipedia.org/wiki/Shunting_yard_algorithm
+// Input array is the user input parsed into an array of tokens
+// Output array is array of tokens in reverse polish notation
+void to_reverse_polish_notation(Token * input, Token * output){
+    Token a;
+    Token b;
+    Token c;
+
+    output[0] = a;
+    output[1] = b;
+    output[2] = c;
+
+
+}
+
+
+void text_to_array_of_tokens(char * userInput, Token * output){
+
+}
---
+++ sw/parser/parser.h
@@ -0,0 +1,26 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+
+/*
+    Token structure (An ordered list of tokens forms an equation)
+
+    - isOperator: 1 for operator, 0 for numerical value
+    - value: The value of this token
+        - If not an operator, value stores numerical value
+        - If operator, the value should store one of the following ascii values: '+', '-', '*', '/', '(', ')'
+    
+*/
+typedef struct token{
+    int isOperator;
+    int value;
+} Token;
+
+
+void parse_equation(char * userInput, Token * output);
+void to_reverse_polish_notation(Token * input, Token * output);
+void text_to_array_of_tokens(char * userInput, Token * output);
+
+#endif /* PARSER_H */
+
+/*** end of file ***/
