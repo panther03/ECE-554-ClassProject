548313690a44689a7b771d776313402500bbb0b8
acfranz2 <acfranz@wisc.edu>
2023-04-17 16:37:56 -0500

    update graphing program

--- fw/calc/graphing/grapher.c
+++ fw/calc/graphing/grapher.c
@@ -1,74 +1,92 @@
-const int VGA_GR_BUFF = 0xfffeD400;
+#include <../solver/solver.h>
 
-/*void plot_x_axis(float lower, float upper) {
+/*volatile int VGA_GR_BUFF = 0xfffeD400;
 
+void plot_x_axis(float lower, float upper) {
+    float span = upper - lower;
+    float y_offset = (upper / span) * 240;
+    
+    int* writeAddr = VGA_GR_BUFF + (int)(y_offset * 320);
+
+    int i;
+    for (i = 0; i < 320; i++) {
+        *(writeAddr + i) = 0xe;
+    }
 }
 
 void plot_y_axis(float lower, float upper) {
+    float span = upper - lower;
+    float x_offset = (-lower / span) * 320;
+    
+    int* writeAddr = VGA_GR_BUFF + (int)(x_offset);
 
+    int i;
+    int y_offset = 0;
+    for (i = 0; i < 240; i++) {
+        *(writeAddr + y_offset) = 0xe;
+	y_offset += 320;
+    }
 }
-
-float get_output(float input, Queue *eq) {
-
-}*/
 
 void plot_xy(int x, int y) {
     int *writeAddr = VGA_GR_BUFF + x + (320 * y);
     *writeAddr = 0x2;
-}
+}*/
 
-__attribute__((section(".text")))
+//__attribute__((section(".text")))
 int main() {
-    /*float x_lower;
-    float x_upper;
+    float x_lower = -10.0f;
+    float x_upper = 10.0f;
     // 320 X 240
     float x_step = (x_upper - x_lower) * .003125f; // * 1/320
 
-    float y_lower;
-    float y_upper;
+    float y_lower = -10.0f;
+    float y_upper = 10.0f;
 
     float x = x_lower;
     float y;
 
+    float span = upper - lower;
+
     // parse eqs;
     Queue* parsedEq;
-    Queue* eqPolishNot[4];
-    char* eq;
+    Queue* eqPolishNot;
+    char* eq = "-1 * x";
     int i;
-    for (i = 0; i < 4; i++) {
+    //for (i = 0; i < 4; i++) {
         parse_equation(eq, parsedEq);
         to_reverse_polish_notation(parsedEq, eqPolishNot[i]);
 
         eq += 80; // eq offset
-    }
+    //}
     
 
     // axis
-    if (x_lower <= 0 && x_upper >= 0) {
-        plot_x_axis(x_lower, x_upper);
+   /* if (x_lower <= 0 && x_upper >= 0) {
+        plot_y_axis(x_lower, x_upper);
     }
     if (y_lower <= 0 && y_upper >= 0) {
-        plot_y_axis(y_lower, y_upper);
-    }
+        plot_x_axis(y_lower, y_upper);
+    }*/
 
     // graph lines
+    int err;
+    int x_coord = 0;
+    int y_coord
     while (x < x_upper) {
-        y = get_output(x, eqPolishNot[0]);
+        y = solveEquation(eqPolishNot[0], x, &err);
 
-	if (y >= y_lower && y <= y_upper) {
-            plot_xy(x, y);
+	if (err != 0 && y >= y_lower && y <= y_upper) {
+            y_coord = (int)((y / span) * 240.0f);
+            // plot_xy(x_coord, y_coord);
+
+	    printf("%d, %d\n", x_coord, y_coord);
         }
 
 	x += x_step;
-    }*/
-    int x = 0;
-    int y = 0;
-
-    int i;
-    int j;
-    for (i = 0; i < 240; i++) {
-        plot_xy(i, i);
+	x_coord += 1;
     }
+    
 
     return 0;
 }
