b6c2d0a5327f122e4742a644fff3ba52d2029e87
acfranz2 <acfranz@wisc.edu>
2023-04-29 13:01:57 -0500

    add grapher without axis correction

--- fw/unit/c/grapher.c
+++ fw/unit/c/grapher.c
@@ -1,4 +1,4 @@
-// #include "../solver/solver.h"
+//#include "solver.h"
 
 //#include <stdio.h>
 
@@ -31,8 +31,85 @@ void plot_y_axis(float lower, float upper) {
 }
 
 void plot_xy(int x, int y) {
+    //printf("%d, %d\n", x, y);
     volatile char *writeAddr = VGA_GR_BUFF + x + (y << 6) + (y << 8);
     *writeAddr = 0x2;
+}
+
+void plotLineLow(int x0, int y0, int x1, int y1) {
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int yi = 1;
+    if (dy < 0) {
+        yi = -1;
+	dy = -dy;
+    }
+
+    int D = (dy << 1) - dx;
+    int y = y0;
+
+    int x;
+    for (x = x0; x <= x1; x++) {
+        // printf("%d, %d\n", x, y);
+        plot_xy(x, y);
+
+	if (D > 0) {
+            y = y + yi;
+            D += (dy - dx) << 1;
+        }
+	else {
+            D += dy << 1;
+        }
+    }
+}
+
+void plotLineHigh(int x0, int y0, int x1, int y1) {
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int xi = 1;
+    if (dx < 0) {
+        xi = -1;
+	dx = -dx;
+    }
+
+    int D = (dx << 1) - dy;
+    int x = x0;
+
+    int y;
+    for (y = y0; y <= y1; y++) {
+        // printf("%d, %d\n", x, y);
+        plot_xy(x, y);
+
+	if (D > 0) {
+            x = x + xi;
+            D += (dx - dy) << 1;
+        }
+	else {
+            D += dx << 1;
+        }
+    }
+}
+
+void plotLine(int x0, int y0, int x1, int y1) {
+    //printf("(%d, %d) -> (%d, %d)\n", x0, y0, x1, y1);
+    int yDiff = y1 - y0;
+    int xDiff = x1 - x0;
+
+    if (yDiff < 0) {
+        yDiff = 0 - yDiff;
+    }
+
+    if (yDiff < xDiff) {
+        plotLineLow(x0, y0, x1, y1);
+    }
+    else {
+        if (y0 > y1) {
+            plotLineHigh(x1, y1, x0, y0);
+        }
+	else {
+            plotLineHigh(x0, y0, x1, y1);
+        }
+    }
 }
 
 __attribute__((section(".text")))
@@ -41,23 +118,21 @@ int main() {
     float x_upper = 10.0f;
     // 320 X 240
     float x_step = (x_upper - x_lower) * .003125f; // * 1/320
+    float x = x_lower;
 
     float y_lower = -10.0f;
     float y_upper = 10.0f;
-
-    float x = x_lower;
     float y;
 
     float span_inv = 1 / (y_upper - y_lower);
     float y_multiplier = span_inv * 239.0f;
-
+/*
     // parse eqs;
-    /*
     Queue parsedEq;
     structureQueue_Queue(&parsedEq);
     Queue eqPolishNot;
     structureQueue_Queue(&eqPolishNot);
-    char* eq = "-1 * x";
+    char* eq = "1.5 * x";
     int i;
     //for (i = 0; i < 4; i++) {
         parse_equation(eq, &parsedEq);
@@ -65,8 +140,7 @@ int main() {
 
         //eq += 80; // eq offset
     //}
-    */
-
+*/
     // axis
     if (x_lower <= 0 && x_upper >= 0) {
         plot_y_axis(x_lower, x_upper);
@@ -77,16 +151,23 @@ int main() {
 
     // graph lines
     int err = 0;
-    int x_coord = 0;
-    int y_coord = 0; 
+    int last_x_coord = 0;
+    y = -x;
+    int last_y_coord = 239 - (int)((y - y_lower) * y_multiplier);
+    int x_coord = 1;
+    int y_coord = 0;
+    x += x_step;
     while (x_coord < 320) {
-        y = -x; //solveEquation(&eqPolishNot, x, &err);
+        y = -x; // solveEquation(&eqPolishNot, x, &err);
 
 	if (err == 0 && y >= y_lower && y <= y_upper) {
             y_coord = 239 - (int)((y - y_lower) * y_multiplier);
-            plot_xy(x_coord, y_coord);
+            plotLine(last_x_coord, last_y_coord, x_coord, y_coord);
+	    //plot_xy(x_coord, y_coord);
         }
 
+	last_x_coord = x_coord;
+	last_y_coord = y_coord;
 	x += x_step;
 	x_coord += 1;
     }
